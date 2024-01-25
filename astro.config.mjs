6effd6c2e6e74d81285a4ed2022dbd12a8190c75
ker <ker@ashampoo.com>
2024-01-25 14:30:25 +0100

    looooooooolollolololol

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://erden-ash.github.io',
+  build: {
+    assetsPrefix: '.'
+  }
 });
